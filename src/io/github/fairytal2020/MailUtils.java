ac934621813f552c79edef923593409e4b46ead2
BuildTools <unconfigured@null.spigotmc.org>
2020-03-26 21:43:15 +0800

    Add gson libs

--- src/io/github/fairytal2020/MailUtils.java
+++ src/io/github/fairytal2020/MailUtils.java
@@ -5,16 +5,22 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
+import java.util.*;
 import java.util.Map.Entry;
 
 
 import microsoft.exchange.webservices.data.core.ExchangeService;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.enumeration.notification.EventType;
 import microsoft.exchange.webservices.data.core.enumeration.property.BodyType;
+import microsoft.exchange.webservices.data.core.enumeration.property.WellKnownFolderName;
 import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
 import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
 import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.notification.GetEventsResults;
+import microsoft.exchange.webservices.data.notification.ItemEvent;
+import microsoft.exchange.webservices.data.notification.PullSubscription;
+import microsoft.exchange.webservices.data.property.complex.FolderId;
 import microsoft.exchange.webservices.data.property.complex.MessageBody;
 
 public class MailUtils {
@@ -70,5 +76,46 @@ public class MailUtils {
     public void send(String subject, String[] to, String[] cc, String bodyText) throws Exception {
         doSend(subject, to, cc, bodyText, null);
     }
+
+    /**
+     * 读取邮件
+     */
+    public void read() throws Exception {
+        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+        ExchangeCredentials credentials = new WebCredentials(user, password);
+        service.setCredentials(credentials);
+        try {
+            service.setUrl(new URI("https://" + mailServer + "/ews/exchange.asmx"));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        List<FolderId> folder = new ArrayList<FolderId>();
+        FolderId folder1 = new FolderId(WellKnownFolderName.Inbox);
+        folder.add(folder1);
+        final PullSubscription subscription = service.subscribeToPullNotifications(folder, 5, null,
+                EventType.NewMail, EventType.Created, EventType.Deleted);
+
+        Integer cacheTime = 1000 * 10;
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    GetEventsResults events = subscription.getEvents();
+
+                    for (ItemEvent itemEvent : events.getItemEvents()) {
+                        if (itemEvent.getEventType() == EventType.NewMail) {
+                            EmailMessage message = EmailMessage.bind(service, itemEvent.getItemId());
+                            System.out.println("######## NEW EMAIL SUBJECT IS: " + message.getSubject());
+                            System.out.println("######## NEW EMAIL CONTENT IS: " + message.getBody());
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, cacheTime);
+    }
+
 }
 
--- src/io/github/fairytal2020/Main.java
+++ src/io/github/fairytal2020/Main.java
@@ -3,6 +3,7 @@ package io.github.fairytal2020;
 public class Main {
     public static void main(String[] args) throws Exception {
         MailUtils mail = new MailUtils("outlook.live.com" , "fairytal2020@outlook.com" , "fairytalbzfx2020");
-        mail.send("Hello World" , new String[]{"wangshengkai2007@163.com" } , new String[]{} , "content");
+        //mail.send("Hello World" , new String[]{"wangshengkai2007@163.com" } , new String[]{} , "content");
+        mail.read();
     }
 }
